4961c956a917870533df7266ad50b52dc24187f9
sakash5940 <akashpatil5940@gmail.com>
2024-10-05 20:08:09 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,39 @@
+// var a="akash patil"
+// var b="sumanth reddy"
+// var c="ganesh"
+// function addtocart(x,y){
+//     var a ="mobile"
+//     console.log(x+y)
+// }
+// addtocart(100,200)
+//TASK:04/10/2024
+function akash(a,b){
+    a=10
+    b=15
+    return a+b
+}
+console.log(akash())
+
+function subract(a,b){
+    a=10
+    b=5
+    return a-b
+}
+console.log(subract())
+
+function multiply(a,b){
+    a=5
+    b=10
+    return a*b
+}
+console.log(multiply())
+
+function divide(a,b){
+    a=10
+    b=0
+    if(b==0){
+        return "cant divided by zero"
+    }
+}
+console.log(divide())
+
